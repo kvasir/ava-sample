a99da8374b536cc9a132b563262940d649d30d8b
John-Philip Johansson <seriema@gmail.com>
2015-11-21 17:19:35 +0100

    Fix formatting

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 import test from 'ava';
 
 test('foo', t => {
-		t.pass();
-		t.end();
+	t.pass();
+	t.end();
 });
 
 test('bar', t => {
-		t.plan(2);
+	t.plan(2);
 
-		setTimeout(() => {
-				t.is('bar', 'bar');
-				t.same(['a', 'b'], ['a', 'b']);
-		}, 100);
+	setTimeout(() => {
+		t.is('bar', 'bar');
+		t.same(['a', 'b'], ['a', 'b']);
+	}, 100);
 });
